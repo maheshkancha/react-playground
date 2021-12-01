512f823b8e6b89292e0a9d5fd6b03a127fd88bbf
Mahesh Bahadur <maheshkancha8@gmail.com>
2021-12-01 15:44:34 +0530

    React template - boiler plate code for webapp development from scratch.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./components/Counter/Counter";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Counter.css";
+
+const Counter = () => {
+  const [count, setCount] = React.useState(0);
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => (prevCount === 0 ? 0 : prevCount - 1));
+  };
+
+  return (
+    <div className="counter-container">
+      <h2>Counter by {process.env.name}</h2>
+      <h3>Count value is {count}</h3>
+      <div>
+        <input
+          type="button"
+          name="decrement"
+          value="Decrement"
+          disabled={count === 0}
+          onClick={decrement}
+        />
+        <input
+          type="button"
+          name="increment"
+          value="Increment"
+          onClick={increment}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
